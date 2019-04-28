e309cd1d5170f7830611eb97423decc10b2a9f20
LuisAraujo96 <luis_araujo96@outlook.com>
2019-04-27 21:28:15 -0300

    fix doc

--- hash.h
+++ hash.h
@@ -8,7 +8,7 @@
  *                DEFINICION DE LOS TIPOS DE DATOS
  * *****************************************************************/
 
-/* Hash está planteado como una hash de punteros genéricos. */
+/* hash está planteado como una hash de punteros genéricos. */
 
 struct hash;	// Definición completa en hash.c.
 typedef struct hash hash_t;
