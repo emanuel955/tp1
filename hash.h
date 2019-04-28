367360cebc6ba765e13e0fa2a6f2cf95fa7fa3a0
LuisAraujo96 <luis_araujo96@outlook.com>
2019-04-27 22:02:49 -0300

    hash.h listo

--- hash.h
+++ hash.h
@@ -27,43 +27,43 @@ typedef void (*hash_destruir_dato_t)(void *);
  *                    	 PRIMITIVAS DEL HASH
  * *****************************************************************/
 
-// Crea una hash.
-// Post: devuelve una nueva hash vacía.
+// Crea un hash.
+// Post: Devuelve un nuevo hash vacío.
 hash_t *hash_crear(hash_destruir_dato_t destruir_dato);
 
 // Guarda un elemento en el hash, si la clave ya se encuentra en la
 // estructura, la reemplaza. De no poder guardarlo devuelve false.
-// Pre: La estructura hash fue inicializada
-// Post: Se almacenó el par (clave, dato)
+// Pre: El hash fue creado.
+// Post: Se almacenó el par (clave, dato).
 bool hash_guardar(hash_t *hash, const char *clave, void *dato);
 
-// Borra un elemento del hash y devuelve el dato asociado.  Devuelve
-// NULL si el dato no estaba.
-// Pre: La estructura hash fue inicializada
-// Post: El elemento fue borrado de la estructura y se lo devolvió,
-// en el caso de que estuviera guardado.
+// Borra un elemento del hash y devuelve el dato asociado. 
+// Devuelve NULL si el dato no estaba.
+// Pre: El hash fue creado.
+// Post: El elemento fue borrado de la estructura y se lo devolvió, en el caso de que estuviera guardado.
 void *hash_borrar(hash_t *hash, const char *clave);
 
 // Obtiene el valor de un elemento del hash, si la clave no se encuentra
 // devuelve NULL.
-// Pre: La estructura hash fue inicializada
+// Pre: El hash fue creado.
+// Post: Obtiene el dato relacionado a la clave.
 void *hash_obtener(const hash_t *hash, const char *clave);
 
-/* Determina si clave pertenece o no al hash.
- * Pre: La estructura hash fue inicializada
- */
+// Determina si la clave pertenece o no al hash.
+// Pre: El hash fue creado.
+// Post: Devuelve true si la clave pertenece, false si no.
 bool hash_pertenece(const hash_t *hash, const char *clave);
 
-/* Devuelve la cantidad de elementos del hash.
- * Pre: La estructura hash fue inicializada
- */
+// Obtiene el valor de la cantidad de elementos del hash. Si el hash tiene elementos,
+// se devuelve la cantidad total. Si está vacio devuelve 0.
+// Pre: El hash fue creado.
+// Post: Se devolvió la cantidad de elementos del hash.
 size_t hash_cantidad(const hash_t *hash);
 
-/* Destruye la estructura liberando la memoria pedida y llamando a la función
- * destruir para cada par (clave, dato).
- * Pre: La estructura hash fue inicializada
- * Post: La estructura hash fue destruida
- */
+// Destruye el hash. Utiliza internamente la función hash_destruir_dato_t,
+// para cada uno de los pares (clave, dato) del hash.
+// Pre: El hash fue creado.
+// Post: Se elimino el hash y todos sus elementos.
 void hash_destruir(hash_t *hash);
 
 /* *****************************************************************
